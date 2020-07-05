67f351eabf60633d8e678b7c005e424a03d75a63
christocarr <christocarr@outlook.com>
2020-07-05 15:06:54 +0100

    Add schema for pet profile

---
+++ models/PetProfile.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const PetProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  animal: {
+    type: String,
+    required: true,
+  },
+  family: {
+    type: String,
+    required: true,
+  },
+  breed: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  bio: {
+    type: String,
+  },
+});
+
+module.exports = PetProfile = mongoose.model('PetProfile', PetProfileSchema);

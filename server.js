6fa1b57a4fbf44af958f475ecc2e895f5ef08363
christocarr <christocarr@outlook.com>
2020-07-05 09:08:08 +0100

    Add routes

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
+app.get('/', (req, res) => res.send('API running'));
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

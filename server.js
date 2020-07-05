3f45fc804a5c9941bc9eb5b107af3d8d94b3987b
christocarr <christocarr@outlook.com>
2020-07-05 08:25:56 +0100

    Add connection to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('API running')
-})
+  res.send('API running');
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
